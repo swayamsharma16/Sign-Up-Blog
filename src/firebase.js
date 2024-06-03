a95a759716f2c8dff8989a98f7fdc1310604440a
Swayam Sharma <93614727+swayamsharma16@users.noreply.github.com>
2024-06-04 00:14:40 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT,
-//   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_APP_FIREBASE_STORAGE_APP_ID,
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export default app;
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
